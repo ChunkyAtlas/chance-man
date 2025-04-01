e600aa7d49728944358ef34cb43f9a89d67fd79e
zukkiers <91077248+zukkiers@users.noreply.github.com>
2025-04-01 11:38:36 +0200

    Update ItemsFilter.java
    Added Most flatspacks
    and all GE item sets

--- src/main/java/com/chanceman/ItemsFilter.java
+++ src/main/java/com/chanceman/ItemsFilter.java
@@ -256,6 +256,214 @@ public class ItemsFilter {
         blocked.add("echo ahrim's robes orn kit (pack of 4)");
         blocked.add("echo virtus robes orn kit (pack of 3)");
         blocked.add("echo venator bow orn kit");
+        
+        // Additional blocked items (Flatpacks and sets NOTE: not all flatpaks were added, some share exact names with their drink counterpart and therefore would block both items)
+        //Flatpacks
+        //blocked.add("armillary sphere"); exists only in game cache
+        //blocked.add("asgarnian ale"); //Flatpack shares name with the drink so can't add it to block list, only ways to fix this is to hardcode it or change blocklist format to IDs or Runelites NameIDS
+        blocked.add("beer barrel");
+        blocked.add("bookcase");
+        blocked.add("carved oak bench");
+        blocked.add("carved oak magic wardrobe");
+        blocked.add("carved oak table");
+        blocked.add("carved teak bench");
+        blocked.add("carved teak magic wardrobe");
+        blocked.add("carved teak table");
+        //blocked.add("celestial globe"); exists only in game cache
+        //blocked.add("chef's delight"); //flatpack shares name with drink
+        //blocked.add("cider barrel"); //flatpack shares name with drink
+        blocked.add("crude chair");
+        //blocked.add("crystal ball"); exists only in game cache
+        //blocked.add("crystal of power"); exists only in game cache
+        //blocked.add("demon lectern"); exists only in game cache
+        //blocked.add("dragon bitter"); //flatpack shares name with drink
+        //blocked.add("eagle lectern"); //exists only in game cache
+        //blocked.add("elemental sphere"); //exists only in game cache
+        blocked.add("fancy teak dresser");
+        blocked.add("four-poster bed");
+        blocked.add("gilded bench");
+        blocked.add("gilded cape rack");
+        blocked.add("gilded clock");
+        blocked.add("gilded dresser");
+        blocked.add("gilded four-poster");
+        blocked.add("gilded magic wardrobe");
+        blocked.add("gilded wardrobe");
+        //blocked.add("globe"); //exists only in game cache
+        //blocked.add("greenman's ale"); //flatpack shares name with drink
+        blocked.add("kitchen table");
+        blocked.add("large oak bed");
+        //blocked.add("large orrery"); //exists only in game cache
+        blocked.add("large teak bed");
+        //blocked.add("lunar globe"); //exists only in game cache
+        blocked.add("m. treasure chest");
+        blocked.add("magic cape rack");
+        blocked.add("mahogany armchair");
+        blocked.add("mahogany armour case");
+        blocked.add("mahogany bench");
+        blocked.add("mahogany bookcase");
+        blocked.add("mahogany cape rack");
+        //blocked.add("mahogany demon"); //exists only in game cache
+        blocked.add("mahogany dresser");
+        //blocked.add("mahogany eagle"); //exists only in game cache
+        blocked.add("mahogany fancy dress box");
+        blocked.add("mahogany magic wardrobe");
+        blocked.add("mahogany table");
+        //blocked.add("mahogany telescope"); //exists only in game cache
+        blocked.add("mahogany toy box");
+        blocked.add("mahogany wardrobe");
+        blocked.add("marble cape rack");
+        blocked.add("marble magic wardrobe");
+        blocked.add("oak armchair");
+        blocked.add("oak armour case");
+        blocked.add("oak bed");
+        blocked.add("oak bench");
+        blocked.add("oak bookcase");
+        blocked.add("oak cape rack");
+        blocked.add("oak chair");
+        blocked.add("oak clock");
+        blocked.add("oak dining table");
+        blocked.add("oak drawers");
+        blocked.add("oak dresser");
+        blocked.add("oak fancy dress box");
+        blocked.add("oak kitchen table");
+        //blocked.add("oak lectern"); //exists only in game cache
+        blocked.add("oak magic wardrobe");
+        blocked.add("oak shaving stand");
+        //blocked.add("oak telescope"); //exists only in game cache
+        blocked.add("oak toy box");
+        blocked.add("oak treasure chest");
+        blocked.add("oak wardrobe");
+        blocked.add("opulent table");
+        //blocked.add("ornamental globe"); //exists only in game cache
+        blocked.add("rocking chair");
+        blocked.add("shaving stand");
+        blocked.add("shoe box");
+        //blocked.add("small orrery"); //exists only in game cache
+        blocked.add("teak armchair");
+        blocked.add("teak armour case");
+        blocked.add("teak bed");
+        blocked.add("teak cape rack");
+        blocked.add("teak clock");
+        //blocked.add("teak demon lectern"); //exists only in game cache
+        blocked.add("teak dining bench");
+        blocked.add("teak drawers");
+        blocked.add("teak dresser");
+        //blocked.add("teak eagle lectern"); //exists only in game cache
+        blocked.add("teak fancy dress box");
+        blocked.add("teak kitchen table");
+        blocked.add("teak magic wardrobe");
+        blocked.add("teak table");
+        //blocked.add("teak telescope"); //exists only in game cache
+        blocked.add("teak toy box");
+        blocked.add("teak treasure chest");
+        blocked.add("teak wardrobe");
+        blocked.add("wood dining table");
+        blocked.add("wooden bed");
+        blocked.add("wooden bench");
+        blocked.add("wooden chair");
+        //GE Sets
+        blocked.add("bronze set (lg)");
+        blocked.add("iron set (lg)");
+        blocked.add("steel set (lg)");
+        blocked.add("black set (lg)");
+        blocked.add("mithril set (lg)");
+        blocked.add("adamant set (lg)");
+        blocked.add("rune armour set (lg)");
+        blocked.add("dragon armour set (lg)");
+        blocked.add("bronze gold-trimmed set (lg)");
+        blocked.add("iron gold-trimmed set (lg)");
+        blocked.add("steel gold-trimmed set (lg)");
+        blocked.add("black gold-trimmed set (lg)");
+        blocked.add("mithril gold-trimmed set (lg)");
+        blocked.add("adamant gold-trimmed set (lg)");
+        blocked.add("rune gold-trimmed set (lg)");
+        blocked.add("gilded armour set (lg)");
+        blocked.add("bronze trimmed set (lg)");
+        blocked.add("iron trimmed set (lg)");
+        blocked.add("steel trimmed set (lg)");
+        blocked.add("black trimmed set (lg)");
+        blocked.add("mithril trimmed set (lg)");
+        blocked.add("adamant trimmed set (lg)");
+        blocked.add("rune trimmed set (lg)");
+        blocked.add("guthix armour set (lg)");
+        blocked.add("saradomin armour set (lg)");
+        blocked.add("zamorak armour set (lg)");
+        blocked.add("ancient rune armour set (lg)");
+        blocked.add("armadyl rune armour set (lg)");
+        blocked.add("bandos rune armour set (lg)");
+        blocked.add("bronze set (sk)");
+        blocked.add("iron set (sk)");
+        blocked.add("steel set (sk)");
+        blocked.add("black set (sk)");
+        blocked.add("mithril set (sk)");
+        blocked.add("adamant set (sk)");
+        blocked.add("rune armour set (sk)");
+        blocked.add("dragon armour set (sk)");
+        blocked.add("bronze gold-trimmed set (sk)");
+        blocked.add("iron gold-trimmed set (sk)");
+        blocked.add("steel gold-trimmed set (sk)");
+        blocked.add("black gold-trimmed set (sk)");
+        blocked.add("mithril gold-trimmed set (sk)");
+        blocked.add("adamant gold-trimmed set (sk)");
+        blocked.add("rune gold-trimmed set (sk)");
+        blocked.add("gilded armour set (sk)");
+        blocked.add("bronze trimmed set (sk)");
+        blocked.add("iron trimmed set (sk)");
+        blocked.add("steel trimmed set (sk)");
+        blocked.add("black trimmed set (sk)");
+        blocked.add("mithril trimmed set (sk)");
+        blocked.add("adamant trimmed set (sk)");
+        blocked.add("rune trimmed set (sk)");
+        blocked.add("guthix armour set (sk)");
+        blocked.add("saradomin armour set (sk)");
+        blocked.add("zamorak armour set (sk)");
+        blocked.add("ancient rune armour set (sk)");
+        blocked.add("armadyl rune armour set (sk)");
+        blocked.add("bandos rune armour set (sk)");
+        blocked.add("initiate harness m");
+        blocked.add("proselyte harness m");
+        blocked.add("proselyte harness f");
+        blocked.add("green dragonhide set");
+        blocked.add("gilded dragonhide set");
+        blocked.add("blue dragonhide set");
+        blocked.add("red dragonhide set");
+        blocked.add("black dragonhide set");
+        blocked.add("guthix dragonhide set");
+        blocked.add("saradomin dragonhide set");
+        blocked.add("zamorak dragonhide set");
+        blocked.add("ancient dragonhide set");
+        blocked.add("armadyl dragonhide set");
+        blocked.add("bandos dragonhide set");
+        blocked.add("ahrim's armour set");
+        blocked.add("dharok's armour set");
+        blocked.add("guthan's armour set");
+        blocked.add("karil's armour set");
+        blocked.add("torag's armour set");
+        blocked.add("verac's armour set");
+        blocked.add("mystic set (blue)");
+        blocked.add("mystic set (light)");
+        blocked.add("mystic set (dark)");
+        blocked.add("mystic set (dusk)");
+        blocked.add("book of balance page set");
+        blocked.add("holy book page set");
+        blocked.add("unholy book page set");
+        blocked.add("book of darkness page set");
+        blocked.add("book of law page set");
+        blocked.add("book of war page set");
+        blocked.add("dwarf cannon set");
+        blocked.add("combat potion set");
+        blocked.add("super potion set");
+        blocked.add("partyhat set");
+        blocked.add("halloween mask set");
+        blocked.add("ancestral robes set");
+        blocked.add("inquisitor's armour set");
+        blocked.add("dagon'hai robes set");
+        blocked.add("justiciar armour set");
+        blocked.add("obsidian armour set");
+        blocked.add("sunfire fanatic armour set");
+        blocked.add("dragonstone armour set");
+        blocked.add("masori armour set (f)");
+        
         BLOCKED_ITEMS = Collections.unmodifiableSet(blocked);
 
         // Initialize the ensouled head mapping.
