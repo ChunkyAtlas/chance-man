f09a13738653cb3fe20486fcde74b66ce52e5d2c
zukkiers <91077248+zukkiers@users.noreply.github.com>
2025-04-03 17:10:24 +0200

    Update ChanceManPlugin.java
    Added Gudiruleset to refreshtradableitems function

--- src/main/java/com/chanceman/ChanceManPlugin.java
+++ src/main/java/com/chanceman/ChanceManPlugin.java
@@ -122,6 +122,7 @@ public class ChanceManPlugin extends Plugin
      * Runs on the client thread. Iterates through item IDs 0–29999 and adds an item to allTradeableItems if:
      * - The item is tradeable and is not blocked by ItemsFilter.
      * - If the "freeToPlay" option is enabled, members-only items are skipped.
+     * - If the "gudiRuleSet" option is enabled, it blocks those items as well from being rollable
      * If the chanceManPanel is initialized, the panel is updated to reflect the refreshed list.
      */
     private void refreshTradeableItems()
@@ -138,6 +139,10 @@ public class ChanceManPlugin extends Plugin
                     {
                         continue;
                     }
+                    if (ItemsFilter.isGudiBlocked(comp.getName()))
+                    {
+                        continue;
+                    }                 
                     allTradeableItems.add(i);
                     count++;
                 }
@@ -151,13 +156,14 @@ public class ChanceManPlugin extends Plugin
     /**
      * Listens for configuration changes
      * When the "freeToPlay" option changes, it refreshes the list of tradeable items.
+     * When the GudiSet option changes, it refreshes tradeable items.
      */
     @Subscribe
     public void onConfigChanged(net.runelite.client.events.ConfigChanged event) {
         if (!event.getGroup().equals("chanceman")) {
             return;
         }
-        if (event.getKey().equals("freeToPlay")) {
+        if (event.getKey().equals("freeToPlay") || event.getKey().equals("gudiRuleSet")) {
             refreshTradeableItems();
         }
     }
