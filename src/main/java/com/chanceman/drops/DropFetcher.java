03f6aba05528f01a61bf5b0f9d5e736282ccb33a
ChunkyAtlas <justinsmith4621@proton.me>
2025-07-02 16:45:51 -0600

    update to v2.2.2

--- src/main/java/com/chanceman/drops/DropFetcher.java
+++ src/main/java/com/chanceman/drops/DropFetcher.java
@@ -147,7 +147,7 @@ public class DropFetcher
     {
         Request req = new Request.Builder()
                 .url(url)
-                .header("User-Agent", "RuneLite-ChanceMan/2.2.1")
+                .header("User-Agent", "RuneLite-ChanceMan/2.2.2")
                 .build();
         try (Response res = httpClient.newCall(req).execute())
         {
